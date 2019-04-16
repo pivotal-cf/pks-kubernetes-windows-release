9021136f9ea287cb1b3e041fab56b2435e523a7c
Ben Moss <bmoss@pivotal.io>
2019-04-16 15:08:54 -0400

    Add support for GCP

---
+++ jobs/flanneld-windows/templates/bin/post-start.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+# add back GCP / AWS metadata server
+<% if p("backend-type") == "win-overlay" %>
+  <% name = "flannel.4096" %>
+<% else %>
+  <% name = "cbr0" %>
+<% end %>
+while (!(Get-HNSNetwork | ? Name -Eq "<%= name %>"))
+{
+    Write-Host "Waiting for overlay network to be enabled"
+    Start-Sleep -sec 1
+}
+route /p add 169.254.169.254 mask 255.255.255.255 0.0.0.0
