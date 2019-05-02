50ce34c2efc92ee883786cdbdc05b22de34f47ad
Ben Moss <bmoss@pivotal.io>
2019-05-02 15:22:45 -0400

    Timeout flannel post-start on failure
    Otherwise the bosh deploy will hang forever

--- jobs/flanneld-windows/templates/bin/post-start.ps1
+++ jobs/flanneld-windows/templates/bin/post-start.ps1
@@ -7,9 +7,19 @@ trap { $host.SetShouldExit(1) }
 <% else %>
   <% name = "cbr0" %>
 <% end %>
-while (!(Get-HNSNetwork | ? Name -Eq "<%= name %>"))
+
+$timeout = 60 # seconds
+$timer = [Diagnostics.Stopwatch]::StartNew()
+
+
+while (!(Get-HNSNetwork | ? Name -Eq "<%= name %>") -and ($timer.Elapsed.TotalSeconds -lt $timeout))
 {
     Write-Host "Waiting for overlay network to be enabled"
     Start-Sleep -sec 1
 }
+$timer.Stop()
+if ($timer.Elapsed.TotalSeconds -gt $timeout) {
+  throw 'timed out waiting for Flannel network to be created'
+}
+
 route /p add 169.254.169.254 mask 255.255.255.255 0.0.0.0
