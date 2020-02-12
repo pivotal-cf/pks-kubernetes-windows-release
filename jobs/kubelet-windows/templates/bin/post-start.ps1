5de23b7b658ca0779457ea181795284e1262bf1b
tenczar <ntenczar@pivotal.io>
2020-02-11 16:59:46 -0800

    Update kubelet-windows to support file arguments (#20)

--- jobs/kubelet-windows/templates/bin/post-start.ps1
+++ jobs/kubelet-windows/templates/bin/post-start.ps1
@@ -16,14 +16,14 @@ function retry($name, $func) {
   do {
     $result=$func.Invoke()
     if ($result) {
-      echo "Successfully $name"
+      Write-Output "Successfully $name"
       return $true
     }
-    echo ("[{0}] Unsuccessful {1}, retrying attempt {2} out of {3}" -f (Get-Date -UFormat %s), $name, $attempt_number, $max_attempts)
+    Write-Output ("[{0}] Unsuccessful {1}, retrying attempt {2} out of {3}" -f (Get-Date -UFormat %s), $name, $attempt_number, $max_attempts)
     $attempt_number=$attempt_number + 1
     sleep 1
   } while ($attempt_number -le $max_attempts)
 
-  echo "Failed all retry attempts for $name"
+  Write-Output "Failed all retry attempts for $name"
   return $false
 }
