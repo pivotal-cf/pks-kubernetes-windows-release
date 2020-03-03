922a733d712970bf0a7457ec461c9a7ed34d9375
Daniel Keeney <dkeeney@pivotal.io>
2020-03-03 11:13:42 -0800

    WIP: use hardcoded setting for proxy
    Co-authored-by: Daniel Keeney <dkeeney@pivotal.io>
    Co-authored-by: Larry Hamel <lhamel@pivotal.io>

--- jobs/docker-windows/templates/pre-start.ps1
+++ jobs/docker-windows/templates/pre-start.ps1
@@ -18,6 +18,12 @@ if (-not $OldPath.Contains($AddedFolder)) {
 
 $mtx.ReleaseMutex()
 
+# TODO for now, hardcode proxy settings for testing; get values from link
+
+SetX HTTP_PROXY "10.87.30.201:8888" /m
+SetX HTTPS_PROXY "10.87.30.201:8888" /m
+SetX NO_PROXY ".internal,.svc,.svc.cluster.local,.svc.cluster,192.167.20.0/16,192.168.20.0/16,10.87.30.0/24" /m
+
 C:\var\vcap\packages\docker-windows\docker\dockerd --register-service
 
 Start-Service Docker
