ca1f53150d41f54fbfa75c6077f8dcd9ec721f72
Larry Hamel <lhamel@pivotal.io>
2020-05-28 13:33:40 -0700

    Vsphere flannel proxy (#47)
    * Set windows proxy infomation when starting docker
    
    [#172093184]
    
    Co-authored-by: Daniel Keeney <dkeeney@pivotal.io>
    Co-authored-by: Shimon Walner <swalner@pivotal.io>
    Co-authored-by: Bridget Arthur <barthur@pivotal.io>
    Co-authored-by: Larry Hamel <lhamel@pivotal.io>

---
+++ jobs/docker-windows/templates/bin/pre-start.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+#remove pid file if one exists from previous running docker
+#TODO check if dockerd proccess is running as well
+$DockerPidPath = "C:\ProgramData\docker\docker.pid"
+if (Test-Path $DockerPidPath) {
+  rm $DockerPidPath 
+}
+
+# Add docker dir to the PATH using a Mutex
+$mtx = New-Object System.Threading.Mutex($false, "PathMutex")
+if (!$mtx.WaitOne(300000)) {
+  throw "Could not acquire PATH mutex"
+}
+
+$AddedFolder= "C:\var\vcap\packages\docker-windows\docker\"
+$OldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+if (-not $OldPath.Contains($AddedFolder)) {
+  $NewPath=$OldPath+';'+$AddedFolder
+  Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
+}
+
+$mtx.ReleaseMutex()
