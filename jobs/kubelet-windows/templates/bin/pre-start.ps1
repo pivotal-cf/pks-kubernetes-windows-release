ccbdfe7622ad5db00986f803d178d0ea6ff58fda
Adam Zhang <adamz@vmware.com>
2021-06-24 07:02:17 +0000

    Sign kubelet certificate locally

--- jobs/kubelet-windows/templates/bin/pre-start.ps1
+++ jobs/kubelet-windows/templates/bin/pre-start.ps1
@@ -26,3 +26,29 @@ else
 {
     Write-Host "$FileName File already exists"
 }
+
+## Sign kubelet certificate
+
+$SIGN_CMD = "C:\var\vcap\packages\local-sign-windows\bin\local-sign-windows.exe"
+$CONFIG_DIR = "C:\var\vcap\jobs\kubelet-windows\config"
+$CA_CONFIG_PATH ="${CONFIG_DIR}\kubelet_ca_config.yml"
+$CERTIFICATE_TEMPLATE_PATH = "${CONFIG_DIR}\kubelet_certificate_template.yml"
+$CERTIFICATE_OUTPUT_DIR = "${CONFIG_DIR}\out"
+$KUBELET_CERT_PATH = "${CONFIG_DIR}\kubelet.pem"
+$KUBELET_KEY_PATH = "${CONFIG_DIR}\kubelet-key.pem"
+
+function do_kubelet_certificate_sign(){
+    # hostname returns something like "WIN-0MM9BP33K43", not the name registered with kube-apiserver, so we read from bosh settings.
+    $NODENAME = (Get-Content "C:\var\vcap\bosh\settings.json" | ConvertFrom-Json).agent_id
+    (Get-Content ${CERTIFICATE_TEMPLATE_PATH}).replace('NODENAME', ${NODENAME}) | Set-Content ${CERTIFICATE_TEMPLATE_PATH}
+    $kubelet_sign_cmd="${SIGN_CMD} certificate --config ${CA_CONFIG_PATH} --template ${CERTIFICATE_TEMPLATE_PATH} --out ${CERTIFICATE_OUTPUT_DIR}"
+    Invoke-Expression ${kubelet_sign_cmd}
+    Move-Item -Force -Path ${CERTIFICATE_OUTPUT_DIR}\certificate.pem -Destination ${KUBELET_CERT_PATH}
+    Move-Item -Force -Path ${CERTIFICATE_OUTPUT_DIR}\key.pem -Destination ${KUBELET_KEY_PATH}
+    rmdir ${CERTIFICATE_OUTPUT_DIR}
+    echo "successfully prepared kubelet certificate."
+}
+
+$NOW = (Get-Date -Format "yyyy-MM-ddTHH:mm:ssZ")
+echo "${NOW}: Start to sign kubelet certificate..."
+do_kubelet_certificate_sign
